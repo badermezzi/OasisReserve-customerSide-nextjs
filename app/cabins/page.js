14f98263b3e5ef3e7b74f5d01f976a374e2e52db
bader <mezzibader159@gmail.com>
2024-10-08 22:30:55 +0100

    making dynamic pages Static to avoid serverless functions calls, handling caching to minimise the server calls

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -4,6 +4,9 @@ import { Spicy_Rice } from "next/font/google";
 import Spinner from "../_components/Spinner";
 
 
+export const revalidate = 3600;
+// export const revalidate = 15;
+
 export const metadata = {
     title: "Cabins",
 };
@@ -21,7 +24,7 @@ export default function Page() {
                 Cozy yet luxurious cabins, located right in the heart of the Italian
                 Dolomites. Imagine waking up to beautiful mountain views, spending your
                 days exploring the dark forests around, or just relaxing in your private
-                hot tub under the stars. Enjoy nature's beauty in your own little home
+                hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
                 away from home. The perfect spot for a peaceful, calm vacation. Welcome
                 to paradise.
             </p>
